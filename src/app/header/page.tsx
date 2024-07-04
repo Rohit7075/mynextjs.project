62f961a9c003f295a67e2b74b22d17b84e35926e
Rohit Dubey <dubeyrohit7076@gmail.com>
2024-07-04 21:39:41 +0530

    deplying next js project

---
+++ src/app/header/page.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+
+const Header = () => (
+  <header id="header" className="header fixed-top" data-scrollto-offset="0">
+    <div className="container-fluid d-flex align-items-center justify-content-between">
+      <Link href="/" className="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <img src="/assets/img/logo.jpg" alt="JBQ Media" className="mx-3" />
+      </Link>
+
+      <nav id="navbar" className="navbar">
+        <ul>
+          <li>
+            <Link href="/" className="nav-link scrollto">Home</Link>
+          </li>
+          <li>
+            <Link href="/#about" className="nav-link scrollto">About</Link>
+          </li>
+          <li>
+            <Link href="/#ourservices" className="nav-link scrollto">Services</Link>
+          </li>
+          {/* <li><Link href="/#clients" className="nav-link scrollto">Clients</Link></li> */}
+          <li>
+            <Link href="/#team" className="nav-link scrollto">Team</Link>
+          </li>
+          {/* <li className="dropdown">
+            <Link href="/industries" className="nav-link"><span>Industries</span> <i className="bi bi-chevron-down dropdown-indicator"></i></Link>
+            <ul>
+              <li><Link href="/industries/dentistry">Dentistry</Link></li>
+              <li><Link href="/industries/chiropractic">Chiropractic</Link></li>
+              <li><Link href="/industries/dermatology">Dermatology</Link></li>
+            </ul>
+          </li> */}
+          {/* <li><Link href="/blog.html">Blog</Link></li> */}
+          <li>
+            <Link href="/#contact" className="nav-link scrollto">Contact</Link>
+          </li>
+        </ul>
+        <i className="bi bi-list mobile-nav-toggle d-none"></i>
+      </nav>
+      {/* .navbar */}
+
+      {/* <a className="btn-getstarted scrollto" href="index.html#about">Get Started</a> */}
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+
+
+"use client";
+import { useEffect } from 'react';
+import aos from '../../public/assets/vendor/aos/aos'
+import Link from 'next/link';
+import Header from './header/page';
+import About from "./about/page"
+import Hero from "./hero/page"
+import Advertisement from "./advertisement/page"
+import Target from "./target/page"
+import Services from "./services/page"
+import Focus from "./focus/page"
+import Faq from "./faq/page"
+import Footer from "./footer/page"
+import Contact from "./contact/page"
+
+
+export default function Home () {
+    useEffect (() => {
+        aos.init();
+    },[])
+    return (
+        <>
+         <Header/>
+         <Hero/>
+         <Target/>
+         <About/>
+         <Advertisement/>
+         <Focus/>
+         <Services/>
+         <Faq/>
+         <Contact/>
+         <Footer/>
+
+        <Link href='#' id='back-to-top' className='back-to-top d-flex align-items-center justify-content-center'>
+                <i className='bi bi-arrow-up-short'></i>
+            </Link>
+            </>
+     
+    )
+}
